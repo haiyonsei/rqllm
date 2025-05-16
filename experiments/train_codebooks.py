fixes for bug that are introduced for masking

experiments/train_codebooks.py
@@ -8,7 +8,7 @@ import argparse
 import sys
 import datetime
 from rqllm.utils import *
-from rqllm.qwkmeans import query_weighted_kmeans
+from rqllm.qwk import query_weighted_kmeans
 from torch import nn
 from rqllm.datasets import create_dataset
 from rqllm.pq import *
@@ -59,7 +59,7 @@ with torch.no_grad():
 def parse_cli():
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('--mode',  choices=['pq', 'rq', 'qwk'], default='rq', help='Training algorithm')
-    p.add_argument('--dataset', choices=['random', 'ptb'], default='random', help='Dataset')
+    p.add_argument('--dataset',type=str,  default='random8', help='Dataset')
     p.add_argument('--M',     type=int, default=256,       help='Codebook size')
     p.add_argument('--n_subvec', type=int, default=2,      help='PQ: number of sub‑vectors')
     p.add_argument('--L',     type=int, default=2,         help='RQ: number of stages')
@@ -90,7 +90,7 @@ def main():
     elif args.mode == 'rq':
         train_codebook_rq(dataset=args.dataset, EPOCH=args.epoch, L=args.L, M=args.M, init=args.init, LR=args.lr, TAU= args.tau)
     elif args.mode == 'qwk':
-        Q_, K_, V_, meta = create_dataset(args.dataset)
+        Q_, K_, V_, meta, mask = create_dataset(args.dataset)
         query_weighted_kmeans(Q_, K_, V_, meta, M=args.M)
 
     # ----- final summary -----

rqllm/datasets.py
@@ -3,13 +3,15 @@ from torch import nn
 from types import SimpleNamespace
 
 def create_dataset(name):
-    if name == 'random':
-        
-        B, T, d_model, n_head = 64, 128, 256, 4
-        #B, T, d_model, n_head = 8, 128, 256, 4
+    if name in ['random64' , 'random8']:
+        if name=='random8':
+            B, T, d_model, n_head = 8, 128, 256, 4
+        else:
+            B, T, d_model, n_head = 64, 128, 256, 4
         d_k = d_model // n_head         # 64
         meta = SimpleNamespace(B=B, T=T, H=n_head, D=d_k)
         with torch.no_grad():
             x = torch.randn(B, T, d_model)
             W_Q, W_K, W_V = nn.Linear(d_model, d_model, bias=False), nn.Linear(d_model, d_model, bias=False), nn.Linear(d_model, d_model, bias=False)
             return W_Q(x), W_K(x), W_V(x), meta, None
+    

rqllm/pq.py
@@ -81,7 +81,7 @@ def train_codebook_core_pq(init_codebooks, Q_, K_, V_, meta, EPOCH=1000, TAU=0.1
 
 
 def train_codebook_pq(dataset, M=256, n_subvec=1, LR=1e-2, EPOCH=1000, TAU=0.1, init='random'):
-    Q_, K_, V_, meta = create_dataset(dataset)
+    Q_, K_, V_, meta, mask = create_dataset(dataset)
     d_k = meta.D
     D_sub = d_k // n_subvec
     #bpa = n_subvec*d_k/math.log2(M)
@@ -99,5 +99,5 @@ def train_codebook_pq(dataset, M=256, n_subvec=1, LR=1e-2, EPOCH=1000, TAU=0.1, 
 
     with torch.no_grad():
         K_hat, _, _ = soft_quantize_pq(K, codebooks, tau=TAU, ste=True)
-        evaluate(Q, K, K_hat, V, d_k)
+        evaluate(Q, K, K_hat, V, d_k, mask=None)
 